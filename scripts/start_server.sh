087954b30e837507df79d3d7944e457259326b98
Vishnu S <58102524+vishnus17@users.noreply.github.com>
2023-06-26 15:33:47 +0530

    update workflow

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,5 +7,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 16
 npm install pm2@latest -g
+pm2_list=$(pm2 list)
+if [ -n "$pm2_list" ]; then
+    pm2 delete all
+    echo "All servers have been deleted."
+else
+    echo "PM2 list is empty. No servers to delete."
+fi
 # source /etc/profile.d/script.sh -> uncomment this to load any environment variables from the script.sh file
 pm2 start index.js # change directory to the location of your server .js file to start your server
