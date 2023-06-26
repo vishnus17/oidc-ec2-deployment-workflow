f34034e94fdfd78d9b2e55d6b967eb64533da670
Vishnu S <58102524+vishnus17@users.noreply.github.com>
2023-06-26 15:06:44 +0530

    update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install --lts
+nvm install 16
 npm install pm2@latest -g
 # source /etc/profile.d/script.sh -> uncomment this to load any environment variables from the script.sh file
 pm2 start index.js
